cf87f8964fdf10e676d85e4f6a13ead602119346
kiyotsuki <sxiguma@yahoo.co.jp>
2017-09-24 22:29:10 +0900

    縦横変更

--- Assets/Script/AdMob.cs
+++ Assets/Script/AdMob.cs
@@ -6,24 +6,21 @@ using GoogleMobileAds.Api;
 
 public class AdMob : MonoBehaviour
 {
-
+	private BannerView bannerView;
 	// Use this for initialization
 	void Start()
-	{
-		RequestBanner();
-	}
-
-	private void RequestBanner()
 	{
 		// 広告ユニット ID を記述します
 		string adUnitId = "ca-app-pub-8131978555342020/3842877758"; // テスト用ID
 
 		// Create a 320x50 banner at the top of the screen.
-		BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+		bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
 		// Create an empty ad request.
-		AdRequest request = new AdRequest.Builder().Build();
+		AdRequest request = new AdRequest.Builder().AddTestDevice("868135021361006").AddTestDevice("868135021392001").Build();
+		
 		// Load the banner with the request.
 		bannerView.LoadAd(request);
+
 
 	}
 }
